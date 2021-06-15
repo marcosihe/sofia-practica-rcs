97925ad9c3962a7be32d0cec288a98de08c70d02
marcosihe <marcosherrera.ib@gmail.com>
2021-06-15 11:43:35 -0300

    Version2 ejercicio2 usando switch

--- EjercicioBucles/Ejercicio2/app.js
+++ EjercicioBucles/Ejercicio2/app.js
@@ -1,4 +1,6 @@
 let numero = parseInt(prompt("Ingrese la nota"));
+
+/*
 if(numero==0 || numero==1 || numero==2){
     alert("Muy deficiente");
 }else if(numero==3 || numero==4){
@@ -12,6 +14,36 @@ if(numero==0 || numero==1 || numero==2){
 }else if(numero==10){
     alert("Sobresaliente");
 }else{
-    alert("La nota ingresada no es valida");
+    alert("Introduce un número válido");
 }
 
+*/
+
+switch(numero){
+    case 0:
+    case 1:
+    case 2:
+        alert("Muy deficiente");
+        break;
+    case 3:
+    case 4:      
+        alert("Insuficiente");
+        break;
+    case 5:
+    case 6:
+        alert("Suficiente");
+        break;
+    case 7:
+        alert("Bien");
+        break;
+    case 8:
+    case 9:
+        alert("Notable");
+        break;
+    case 10:
+        alert("Sobresaliente");
+        break;
+    default:
+        alert("Introduce un numero valido");
+        break;    
+}
